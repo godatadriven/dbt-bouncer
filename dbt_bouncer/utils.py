f0a29c9a21ed6201c13bdc4b3d51b3c9b379c4fb
pgoslatara <pgoslatara@gmail.com>
2024-07-24 17:06:20 +0200

    Adding test for meta config in sources

---
+++ dbt_bouncer/utils.py
@@ -0,0 +1,16 @@
+def flatten(structure, key="", path="", flattened=None):
+    """
+    Take a dict of arbitrary depth that may contain lists and return a non-nested dict of all pathways.
+    """
+
+    if flattened is None:
+        flattened = {}
+    if type(structure) not in (dict, list):
+        flattened[((path + ">") if path else "") + key] = structure
+    elif isinstance(structure, list):
+        for i, item in enumerate(structure):
+            flatten(item, "%d" % i, path + ">" + key, flattened)
+    else:
+        for new_key, value in structure.items():
+            flatten(value, new_key, path + ">" + key, flattened)
+    return flattened
