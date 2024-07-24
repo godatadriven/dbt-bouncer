a75e080a80ab9c8595b915105fa362a431de155a
pgoslatara <pgoslatara@gmail.com>
2024-07-24 17:51:57 +0200

    Add marks to pytest

--- tests/conftest.py
+++ tests/conftest.py
@@ -6,7 +6,19 @@ from dbt_artifacts_parser.parser import parse_manifest
 
 
 def pytest_configure(config):
-    config.addinivalue_line("markers", "iterate_over_models: Tests that should run once per model")
+    config.addinivalue_line("markers", "iterate_over_macros: Tests that should run once per macro")
+    config.addinivalue_line(
+        "markers",
+        "iterate_over_models: Tests that should run once per model",
+    )
+    config.addinivalue_line(
+        "markers",
+        "iterate_over_sources: Tests that should run once per source",
+    )
+    config.addinivalue_line(
+        "filterwarnings",
+        "ignore::UserWarning",
+    )
 
 
 @pytest.fixture(scope="session")
