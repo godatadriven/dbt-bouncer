8f0f4a5493374d3e6354d48db9f4bd416373aeaa
Padraic Slattery <pgoslatara@gmail.com>
2025-09-11 20:44:36 +0200

    Adding checks on model versions

--- src/dbt_bouncer/checks/manifest/check_models.py
+++ src/dbt_bouncer/checks/manifest/check_models.py
@@ -709,6 +709,39 @@ class CheckModelHasUnitTests(BaseCheck):
             )
 
 
+class CheckModelLatestVersionSpecified(BaseCheck):
+    r"""Check that the `latest_version` attribute of the model is set.
+
+    Receives:
+        model (DbtBouncerModelBase): The DbtBouncerModelBase object to check.
+
+    Other Parameters:
+        description (Optional[str]): Description of what the check does and why it is implemented.
+        exclude (Optional[str]): Regex pattern to match the model path. Model paths that match the pattern will not be checked.
+        include (Optional[str]): Regex pattern to match the model path. Only model paths that match the pattern will be checked.
+        severity (Optional[Literal["error", "warn"]]): Severity level of the check. Default: `error`.
+
+    Example(s):
+        ```yaml
+        manifest_checks:
+            - name: check_model_latest_version_specified
+              include: ^models/marts
+        ```
+
+    """
+
+    model_config = ConfigDict(extra="forbid", protected_namespaces=())
+
+    model: "DbtBouncerModelBase" = Field(default=None)
+    name: Literal["check_model_latest_version_specified"]
+
+    def execute(self) -> None:
+        """Execute the check."""
+        assert self.model.latest_version is not None, (
+            f"`{self.model.name}` does not have a specified `latest_version`."
+        )
+
+
 class CheckModelMaxChainedViews(BaseCheck):
     """Models cannot have more than the specified number of upstream dependents that are not tables.
 
@@ -1148,6 +1181,99 @@ class CheckModelSchemaName(BaseCheck):
             is not None
         ), (
             f"`{self.model.schema_}` does not match the supplied regex `{self.schema_name_pattern.strip()})`."
+        )
+
+
+class CheckModelVersionAllowed(BaseCheck):
+    r"""Check that the version of the model matches the supplied regex pattern.
+
+    Parameters:
+        version_pattern (str): Regexp the version must match.
+
+    Receives:
+        model (DbtBouncerModelBase): The DbtBouncerModelBase object to check.
+
+    Other Parameters:
+        description (Optional[str]): Description of what the check does and why it is implemented.
+        exclude (Optional[str]): Regex pattern to match the model path. Model paths that match the pattern will not be checked.
+        include (Optional[str]): Regex pattern to match the model path. Only model paths that match the pattern will be checked.
+        severity (Optional[Literal["error", "warn"]]): Severity level of the check. Default: `error`.
+
+    Example(s):
+        ```yaml
+        manifest_checks:
+            - name: check_model_version_allowed
+              include: ^models/marts
+              version_pattern: >- # Versions must be numeric
+                [0-9]\d*
+            - name: check_model_version_allowed
+              include: ^models/marts
+              version_pattern: ^(stable|latest)$ # Version can be "stable" or "latest", nothing else is permitted
+        ```
+
+    """
+
+    model_config = ConfigDict(extra="forbid", protected_namespaces=())
+
+    model: "DbtBouncerModelBase" = Field(default=None)
+    name: Literal["check_model_version_allowed"]
+    version_pattern: str
+
+    def execute(self) -> None:
+        """Execute the check."""
+        if self.model.version:
+            assert (
+                re.compile(self.version_pattern.strip()).match(str(self.model.version))
+                is not None
+            ), (
+                f"Version `{self.model.version}` in `{self.model.name}` does not match the supplied regex `{self.version_pattern.strip()})`."
+            )
+
+
+class CheckModelVersionPinnedInRef(BaseCheck):
+    r"""Check that the version of the model is always specified in downstream nodes.
+
+    Receives:
+        manifest_obj (DbtBouncerManifest): The DbtBouncerManifest object parsed from `manifest.json`.
+        model (DbtBouncerModelBase): The DbtBouncerModelBase object to check.
+
+    Other Parameters:
+        description (Optional[str]): Description of what the check does and why it is implemented.
+        exclude (Optional[str]): Regex pattern to match the model path. Model paths that match the pattern will not be checked.
+        include (Optional[str]): Regex pattern to match the model path. Only model paths that match the pattern will be checked.
+        severity (Optional[Literal["error", "warn"]]): Severity level of the check. Default: `error`.
+
+    Example(s):
+        ```yaml
+        manifest_checks:
+            - name: check_model_version_pinned_in_ref
+              include: ^models/marts
+        ```
+
+    """
+
+    model_config = ConfigDict(extra="forbid", protected_namespaces=())
+
+    manifest_obj: "DbtBouncerManifest" = Field(default=None)
+    model: "DbtBouncerModelBase" = Field(default=None)
+    name: Literal["check_model_version_pinned_in_ref"]
+
+    def execute(self) -> None:
+        """Execute the check."""
+        downstream_models = [
+            x
+            for x in self.manifest_obj.manifest.child_map[self.model.unique_id]
+            if x.startswith("model.")
+        ]
+        downstream_models_with_unversioned_refs: List[str] = []
+        for m in downstream_models:
+            downstream_models_with_unversioned_refs.extend(
+                m
+                for ref in self.manifest_obj.manifest.nodes[m].refs
+                if ref.name == self.model.unique_id.split(".")[-2] and not ref.version
+            )
+        assert not downstream_models_with_unversioned_refs, (
+            f"`{self.model.name}` is referenced without a pinned version in downstream models: {downstream_models_with_unversioned_refs}."
         )
 
 
