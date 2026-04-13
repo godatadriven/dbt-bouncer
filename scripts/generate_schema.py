6fcfc82a4b3926099452c816a0d3505b45b716ae
Matteo Renoldi <matteo@renoldi.org>
2026-04-13 14:10:59 +0200

    refactor: script update

--- scripts/generate_schema.py
+++ scripts/generate_schema.py
@@ -100,7 +100,7 @@ def main() -> None:
     import dbt_bouncer.checks.manifest  # noqa: F401
     import dbt_bouncer.checks.run_results  # noqa: F401
     from dbt_bouncer.checks.common import NestedDict
-    from dbt_bouncer.config_file_parser import create_bouncer_conf_class
+    from dbt_bouncer.configuration_file.parser import create_bouncer_conf_class
 
     logging.info("Building DbtBouncerConf model with all checks...")
     DbtBouncerConf = create_bouncer_conf_class()  # noqa: N806
